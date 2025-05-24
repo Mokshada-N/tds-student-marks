eeed60b1016b68a2b2124344e04a37405c6948d3
Mokshada-N <24f1000656@ds.study.iitm.ac.in>
2025-05-24 22:40:25 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,21 +4,17 @@ from typing import List
 import json
 import os
 
-# Initialize FastAPI app
 app = FastAPI()
 
-# Enable CORS for all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["GET"],  # Only allow GET requests
+    allow_methods=["GET"],  
     allow_headers=["*"],
 )
 
-# Get the directory of the current file
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# Load student marks data from the same directory as this file
 with open(os.path.join(current_dir, 'q-vercel-python.json')) as f:
     students_data = json.load(f)
 
@@ -33,7 +29,6 @@ async def get_marks(name: List[str] = Query(None)):
     
     marks = []
     for student_name in name:
-        # Look for the student in the data
         mark = next((student["marks"] for student in students_data 
                      if student["name"].lower() == student_name.lower()), None)
         marks.append(mark)
